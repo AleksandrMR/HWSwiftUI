fa4b4f37a1b9b62ef9b88ba044e83da4a980bab3
Aleksandr Milashevski <aleksandr.milashevski.ios@gmail.com>
2021-03-09 08:55:25 +0100

    001. First commit

---
+++ HWSwiftUI/HWSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HWSwiftUIApp.swift
+//  HWSwiftUI
+//
+//  Created by Aleksandr Milashevski on 09/03/21.
+//
+
+import SwiftUI
+
+@main
+struct HWSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
